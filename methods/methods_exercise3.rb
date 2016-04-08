4a020c3124b46ac44b6cf9099a609ab3b30b2e8b
Amy Woodworth <woodworthcommunications@gmail.com>
2016-04-08 16:46:46 -0500

    Finish method exercises.

---
+++ methods/methods_exercise3.rb
@@ -0,0 +1,11 @@
+# Write a program that includes a method called multiply that takes two arguments and returns
+# the product of the two numbers.
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+p multiply(10, 10)
+p multiply(5, 2)
+p multiply(-1, 20)
+
---
+++ methods/methods_exercise4.rb
@@ -0,0 +1,12 @@
+# What will the following code print to the screen?
+
+# def scream(words)
+#   words = words + "!!!!"
+#   return
+#   puts words
+# end
+
+# scream("Yippeee")
+
+puts "Doesn't print anything because the 'return' ends the method call before it can 'puts words'."
+
